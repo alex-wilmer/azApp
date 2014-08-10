008f3f0bbf411a4cbf0a7aba508e64c22ff53fe7
Alex Wilmer <dhazium@gmail.com>
2014-08-10 19:03:48 -0400

    socket

---
+++ server.js
@@ -0,0 +1,30 @@
+//SERVER SETUP
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+var fs = require('fs');
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+//SERVER ROUTING
+//serve text
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res){
+	res.sendfile('index.html');
+});
+
+io.on('connection', function (socket) {
+	//socket.on('writeFile', function(data) {
+		var date = new Date();
+		var data = date.getFullYear() + date.getSeconds();
+		fs.writeFile('message.txt', data, function (err) {
+		  if (err) throw err;
+		  console.log('It\'s saved!');
+		});
+	//});
+});
+
